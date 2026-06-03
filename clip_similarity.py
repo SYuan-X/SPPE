c43ecdf7d93b5117d4da4880fca8f5bef263d3a7
SYuan-X <136813649+SYuan-X@users.noreply.github.com>
2026-06-03 13:28:48 +0800

    Clean up commented-out code in clip_similarity.py
    Removed commented-out code for various similarity functions and image processing methods.

--- clip_similarity.py
+++ clip_similarity.py
@@ -56,36 +56,6 @@ class ClipSimilarity(nn.Module):
         psnr_score = psnr(img1_np, img2_np, data_range=1.0)
         return ssim_score,psnr_score 
 
-    # def forward(self, image_0: torch.Tensor, image_1: torch.Tensor, text: list[str]) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-    #     image_features_0 = self.encode_image(image_0)
-    #     image_features_1 = self.encode_image(image_1)
-    #     text_features = self.encode_text(text)
-    #     # text_features_1 = self.encode_text(text_1)
-    #     sim_0 = F.cosine_similarity(image_features_0, text_features)
-    #     sim_1 = F.cosine_similarity(image_features_1, text_features)
-    #     sim_direction = F.cosine_similarity(image_features_1 - image_features_0, text_features_1 - text_features_0)
-    #     sim_image = F.cosine_similarity(image_features_0, image_features_1)
-
-    #     return sim_0, sim_1, sim_direction, sim_image
-    # def image_with_image(self, image_0: torch.Tensor, image_1: torch.Tensor) -> torch.Tensor:
-    #     image_features_0 = self.encode_image(image_0)
-    #     image_features_1 = self.encode_image(image_1)
-    #     # text_features = self.encode_text(text)
-    #     # text_features_1 = self.encode_text(text_1)
-    #     sim_0 = F.cosine_similarity(image_features_0, image_features_1)
-    #     return sim_0
-
-    # def images_with_text(self, image_0: torch.Tensor, image_1: torch.Tensor) -> torch.Tensor:
-    #     image_features_0 = self.encode_image(image_0)
-    #     image_features_1 = self.encode_image(image_1)
-    #     # text_features = self.encode_text(text)
-    #     # text_features_1 = self.encode_text(text_1)
-    #     sim_0 = F.cosine_similarity(image_features_0, image_features_1)
-    #     return sim_0
-    
-        # sim_1 = F.cosine_similarity(image_features_1, text_features_1)
-        # sim_direction = F.cosine_similarity(image_features_1 - image_features_0, text_features_1 - text_features_0)
-        # sim_image = F.cosine_similarity(image_features_0, image_features_1)
     def masked_image(self, img1: torch.Tensor, mask: torch.Tensor):
     # img1, img2: (1, 3, H, W)
     # mask: (1, 1, H, W), values in {0, 1}
@@ -99,22 +69,6 @@ class ClipSimilarity(nn.Module):
 
 
     def forward(self, exemplar_image_0, exemplar_image_1, image_0, test_1, image_1,text):
-        # masked_image_0,reverse_masked_image_0 = self.masked_image(image_0,mask)
-        # masked_test_1,reverse_masked_test_1 = self.masked_image(test_1,mask)
-        # masked_exemplar_image_0,reverse_masked_exemplar_image_0 = self.masked_image(exemplar_image_0,mask)
-        # masked_exemplar_image_1,reverse_masked_exemplar_image_1 = self.masked_image(exemplar_image_1,mask)
-        # masked_image_features_0 = self.encode_image(masked_image_0)
-        # masked_test_features_1 = self.encode_image(masked_test_1)
-        # reverse_masked_image_features_0 = self.encode_image(reverse_masked_image_0)
-        # reverse_masked_test_features_1 = self.encode_image(reverse_masked_test_1)
-        # reverse_masked_exemplar_image_features_0 = self.encode_image(reverse_masked_exemplar_image_0)
-        # reverse_masked_exemplar_image_features_1 = self.encode_image(reverse_masked_exemplar_image_1)
-
-
-        # masked_sim_src_keep = F.cosine_similarity(masked_test_features_1, masked_image_features_0)
-        # masked_direction_sim = F.cosine_similarity(reverse_masked_exemplar_image_features_1 - reverse_masked_exemplar_image_features_0, reverse_masked_test_features_1 - reverse_masked_image_features_0)
-        # masked_ssim_score, masked_psnr_score  = self.source_keep(masked_image_0, masked_test_1)
-
         exemplar_image_features_0 = self.encode_image(exemplar_image_0)
         exemplar_image_features_1 = self.encode_image(exemplar_image_1)
         image_features_0 = self.encode_image(image_0)
@@ -124,17 +78,9 @@ class ClipSimilarity(nn.Module):
 
         # test image with text(transfer model performance)
         sim_test_text=0
-        # sim_test_text = F.cosine_similarity(test_features_1, text_features)
-        # exemplar image with text(original performance of LVLM)
         sim_src_text=0
-        # sim_src_text = F.cosine_similarity(image_features_1, text_features)
-        # consistency_score_fix = 1 - torch.abs(sim_src_text - sim_test_text)
-
-        # keep source features after transfer
-        # sim_src_keep = (F.cosine_similarity(test_features_1, image_features_0) + 2*masked_sim_src_keep)/3
         sim_src_keep = F.cosine_similarity(test_features_1, image_features_0)
 
-        # 和gt图片的相似度
         consistency_score_gt = F.cosine_similarity(test_features_1, image_features_1)
 
         # transfer consistency with LVLM(生成的图片与text吻合度相较于直接使用LVLM差别有多少)
@@ -145,15 +91,9 @@ class ClipSimilarity(nn.Module):
         # direction_sim = (F.cosine_similarity(exemplar_image_features_1 - exemplar_image_features_0, test_features_1 - image_features_0) + 2*masked_direction_sim)/3
         direction_sim = F.cosine_similarity(exemplar_image_features_1 - exemplar_image_features_0, test_features_1 - image_features_0)
 
-        # 是否保留了原图的结构等特征
         ssim_score, psnr_score  = self.source_keep(image_0, test_1) 
         # masked_ssim_score, masked_psnr_score  = self.source_keep(masked_image_0, masked_test_1)
 
-        # ssim_score = (ssim_score+2*masked_ssim_score)/3
-        # psnr_score = (psnr_score+2*masked_psnr_score)/3
-        # sim_src_keep = masked_sim_src_keep
-        # direction_sim = masked_direction_sim
-        # ssim_score, psnr_score = masked_ssim_score, masked_psnr_score
         ssim_score_gt, psnr_score_gt = self.source_keep(test_1, image_1) 
 
         return sim_src_keep, ssim_score,psnr_score ,sim_src_text, sim_test_text, consistency_score, direction_sim,consistency_score_gt, ssim_score_gt, psnr_score_gt
